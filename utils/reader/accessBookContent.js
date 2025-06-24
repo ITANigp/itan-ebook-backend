f847a206f63334bdde6bd156aa5e115e965b97f8
Ol-create <oluola96@gmail.com>
2025-06-24 17:27:40 +0100

    split the app for author only

--- utils/reader/accessBookContent.js
+++
@@ -1,8 +0,0 @@
-export const accessBookContent = async (readingToken, bookId) => {
-  const response = await API.get(`/books/${bookId}/content`, {
-    headers: {
-      Authorization: `Bearer ${readingToken}`,
-    },
-  });
-  return response.data;
-};
--- utils/reader/listReaderPurchase.js
+++
@@ -1,8 +0,0 @@
-export const listUserPurchases = async (token) => {
-  const response = await API.get("/purchases", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
-};
