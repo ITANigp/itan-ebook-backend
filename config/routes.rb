d157830b83164a0250dee4d99efa4665118dbd2a
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-20 17:38:07 +0100

    Remove reading token controller completely

--- config/routes.rb
+++ config/routes.rb
@@ -130,8 +130,7 @@ Rails.application.routes.draw do
 
       #Reviews & likes
       resources :reviews, only: [:create, :destroy]
-      resources :likes, only: [:index, :create, :destroy]
-      # resources :reading_tokens, only: [:create]      
+      resources :likes, only: [:index, :create, :destroy]        
       resource :direct_uploads, only: [:create]
     end
   end
