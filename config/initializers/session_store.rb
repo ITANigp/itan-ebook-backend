2cf653adbd03fe9d4a1f758f3629868a2bf85fa8
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-25 18:24:57 +0100

    Remove unnecessary file

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-# Cookie configuration for session-based authentication
-Rails.application.config.session_store :cookie_store,
-  key: '_itan_session',
-  same_site: Rails.env.production? ? :none : :lax,
-  secure: Rails.env.production?,
-  httponly: true
