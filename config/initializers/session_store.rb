1178e5093b166d47d2ee22e492bbe4e176b5a28d
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-29 05:16:46 +0100

    Use database as persistent session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
-Rails.application.config.session_store :cookie_store,
+Rails.application.config.session_store :active_record_store,
   key: '_itan_session',
   same_site: :none,
   secure: true,
   httponly: true,
-  domain: '.itan.app'
+  domain: '.itan.app',
+  expire_after: 14.days
