711766fe0fad66649f41add7a3a11acf1646d36b
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-30 12:10:58 +0100

    Update kyc for accepted terms

--- app/controllers/api/v1/authors/kyc_controller.rb
+++ app/controllers/api/v1/authors/kyc_controller.rb
@@ -2,9 +2,10 @@ class Api::V1::Authors::KycController < ApplicationController
   before_action :authenticate_author!
 
   def update_step
-    kyc_step = params[:author].try(:[], :kyc_step) || params[:kyc].try(:[], :author).try(:[], :kyc_step)
-    if current_author.update(kyc_step: kyc_step)
-      render json: { status: 200, kyc_step: current_author.kyc_step }
+    kyc_step = params[:author][:kyc_step]
+    accepted_terms = params[:author][:accepted_terms]
+    if current_author.update(kyc_step: kyc_step, accepted_terms: accepted_terms)
+      render json: { status: 200, kyc_step: current_author.kyc_step, accepted_terms: current_author.accepted_terms }
     else
       render json: { status: 422, errors: current_author.errors.full_messages }, status: :unprocessable_entity
     end
