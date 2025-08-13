139d97866592a05e118b4d6780cf6e34587a9bf5
Moise Mulungu <your-email@example.com>
2025-08-13 06:06:12 +0200

    generate slug-based author/book link with admin approval handling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_04_154601) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_13_031742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -161,7 +161,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_04_154601) do
     t.index ["categories"], name: "index_books_on_categories", using: :gin
     t.index ["contributors"], name: "index_books_on_contributors", using: :gin
     t.index ["keywords"], name: "index_books_on_keywords", using: :gin
-    t.index ["slug"], name: "index_books_on_slug", unique: true
+    t.index ["slug"], name: "index_books_on_slug", unique: true, where: "(slug IS NOT NULL)"
     t.index ["tags"], name: "index_books_on_tags", using: :gin
     t.index ["unique_audio_id"], name: "index_books_on_unique_audio_id", unique: true
     t.index ["unique_book_id"], name: "index_books_on_unique_book_id", unique: true
