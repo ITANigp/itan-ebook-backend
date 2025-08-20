16ec80b0ba09967ccd5481b674a4415babe815c4
Ayokunnumi1 <omololuayk@gmail.com>
2025-08-20 12:21:49 +0100

    Update slug link column

--- db/migrate/20250813031741_add_slug_to_books.rb
+++ db/migrate/20250813031741_add_slug_to_books.rb
@@ -1,5 +1,7 @@
 class AddSlugToBooks < ActiveRecord::Migration[7.1]
   def change
-    add_column :books, :slug, :string
+    unless column_exists?(:books, :slug)
+      add_column :books, :slug, :string
+    end
   end
 end
