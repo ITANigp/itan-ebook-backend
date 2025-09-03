d81035b133bdf1291e6d065c910da0cfb6fbecd6
Moise Mulungu <your-email@example.com>
2025-09-03 23:50:20 +0200

    Added new attributes to BookSummarySerializer

--- app/serializers/book_summary_serializer.rb
+++ app/serializers/book_summary_serializer.rb
@@ -1,7 +1,10 @@
 class BookSummarySerializer
   include JSONAPI::Serializer
 
-  attributes :title, :slug, :categories, :approval_status
+  attributes :title, :slug, :categories, :approval_status,
+             :description, :total_pages, :publication_date, :cover_image_url,
+             :ebook_file_size_human, :author, :average_rating,
+             :ebook_price, :likes_count, :reviews, :reviews_count
 
   attribute :cover_image_url do |book|
     Rails.application.routes.url_helpers.url_for(book.cover_image) if book.cover_image.attached?
@@ -16,6 +19,27 @@ class BookSummarySerializer
 
   attribute :average_rating do |book|
     book.reviews.average(:rating)&.round(2) || 0
+  end
+
+  attribute :reviews_count do |book|
+    book.reviews.count
+  end
+
+  attribute :reviews do |book|
+    book.reviews.map do |review|
+      {
+        id: review.id,
+        comment: review.comment,
+        rating: review.rating,
+        reviewer: if review.reader.respond_to?(:full_name)
+                    review.reader.full_name
+                  elsif review.reader.respond_to?(:first_name) && review.reader.respond_to?(:last_name)
+                    "#{review.reader.first_name} #{review.reader.last_name}".strip
+                  else
+                    review.reader.email
+                  end
+      }
+    end
   end
 
   attribute :likes_count do |book|
