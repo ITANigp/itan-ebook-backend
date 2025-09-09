17bf982ccd502e52d7645a13e0e21eecff623bac
Ayokunnumi1 <omololuayk@gmail.com>
2025-09-09 11:13:58 +0100

    Add ebook file size to storefront serializer

--- app/serializers/storefront_book_serializer.rb
+++ app/serializers/storefront_book_serializer.rb
@@ -1,7 +1,7 @@
 class StorefrontBookSerializer
   include JSONAPI::Serializer
 
-  attributes :title, :description, :created_at, :total_pages, :categories
+  attributes :title, :description, :created_at, :total_pages, :categories, :ebook_file_size_human
 
   attribute :cover_image_url do |book|
     Rails.application.routes.url_helpers.url_for(book.cover_image) if book.cover_image.attached?
